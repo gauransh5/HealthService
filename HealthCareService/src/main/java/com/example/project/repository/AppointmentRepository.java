afdefd86f222069dbab959d171170929fd143360
gauransh5 <gauransh5@gmail.com>
2021-07-09 16:41:59 +0530

    First Signed-off-by: gauransh5 <gauransh5@gmail.com>

---
+++ HealthCareService/src/main/java/com/example/project/repository/AppointmentRepository.java
@@ -0,0 +1,10 @@
+﻿package com.example.project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.project.Model.Appointment;
+
+public interface AppointmentRepository extends JpaRepository<Appointment,String>{
+
+}
+
---
+++ HealthCareService/src/main/java/com/example/project/repository/PatientRepository.java
@@ -0,0 +1,15 @@
+﻿package com.example.project.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.project.Model.Appointment;
+import com.example.project.Model.Patient;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient,String>{
+
+}
+
